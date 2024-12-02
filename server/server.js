1c9f57b98bb5168a863e7101db594f0fc8a23bf5
Berrlin <94164744+Berrlin@users.noreply.github.com>
2024-12-02 14:37:23 +0700

    update cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express();
 const port = process.env.PORT||4000;
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    origin: 'https://maiquoctuan.io.vn',
+    credentials: true,
+))
 connectDB();
 
 app.use("/api/user",userRoute)
