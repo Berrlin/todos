d6fff5b1e4e04f743c08b443237011663f76a172
Berrlin <94164744+Berrlin@users.noreply.github.com>
2024-12-04 17:14:14 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,29 +1,28 @@
-import express from "express";
-import cors from "cors";
-import https from "https";
-import fs from "fs";
+import express from "express"
+import cors from "cors"
 import { connectDB } from "./config/db.js";
 import userRoute from "./routes/UserRoute.js";
+import 'dotenv/config'
 import todosRoute from "./routes/TodosRoute.js";
-import 'dotenv/config';
+
+
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT||4000;
 
-
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 connectDB();
 
-// Routes
-app.use("/api/user", userRoute);
-app.use("/api/todos", todosRoute);
+app.use("/api/user",userRoute)
+app.use("/api/todos", todosRoute)
 
-app.get("/", (req, res) => {
-    res.send("API Working");
-});
 
-// Tạo HTTPS server
-https.createServer(options, app).listen(port, () => {
-    console.log(`HTTPS Server Started on https://maiquoctuan.io.vn:${port}`);
-});
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+//callback function ()=>
+app.listen(port,()=>{
+    console.log(Server Started on http://localhost:${port})
+})
